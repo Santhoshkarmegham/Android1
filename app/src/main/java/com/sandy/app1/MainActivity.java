59100aa2dadaf8835b179214f257eef5a6888d1b
santhosh-vk <62786206+Santhoshkarmegham@users.noreply.github.com>
2021-05-30 21:46:33 +0530

    Register

--- app/src/main/java/com/sandy/app1/MainActivity.java
+++ app/src/main/java/com/sandy/app1/MainActivity.java
@@ -3,6 +3,8 @@ package com.sandy.app1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView front = findViewById(R.id.txthello);
+
+
+    }
+// day 01 activity kearning the basics
+    public void onBtnclick(View view){
+
+        TextView first = findViewById(R.id.fname);
+        TextView last = findViewById(R.id.lname);
+        TextView surname = findViewById(R.id.sname);
+
+        EditText fname = findViewById(R.id.FirstName);
+        EditText lname = findViewById(R.id.Latname);
+        EditText sname = findViewById(R.id.Surname);
+
+
+
+
+        first.setText("FirstName" + fname.getText().toString());
+        last.setText("FirstName" + lname.getText().toString());
+        surname.setText("FirstName" + sname.getText().toString());
+
+
     }
 }
